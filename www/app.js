a1f61ccc58ddd41d1508b892661f5dba549c6c72
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-04 18:13:13 -0500

    Ajout d'une entete dynamique et des balises meta

--- www/app.js
+++ www/app.js
@@ -2,19 +2,62 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
-app.use('/assets', express.static(__dirname + "/public"), function(req,res){
-    console.log(req.url);
-});
+app.use('/assets', express.static(__dirname + "/public"));
 
 app.get('/', (req,res)=>{
-    console.log(req.url);
-    var data = {test:"test1"};
-    res.render('index', data);
+    let data = {};
+    data.infosPage={
+        title:"Maxime Lacasse Germain | Portfolio",
+        description:"Portfolio de Maxime Lacasse Germain contenant les dernières réalisations en web, jeux vidéo et multimédia.",
+        robots:"INDEX, FOLLOW",
+        keywords:"Web, jeux vidéo, portfolio"
+    };
+    res.render('index', {data:data});
 });
 
-app.get('/web/:travail', (req,res)=>{
-    var data = {test:req.params.travail};
-    res.render('index', data);
+app.get('/web',(req,res)=>{
+    let data = {};
+    data.infosPage={
+        title:"",
+        description:"",
+        robots:"",
+        keywords:""
+    };
+    res.render('web', data);
 });
+
+app.get('/jeuxvideo',(req,res)=>{
+    let data = {};
+    data.infosPage={
+        title:"",
+        description:"",
+        robots:"",
+        keywords:""
+    };
+    res.render('jeuxvideo', data);
+});
+
+app.get('/multimedias',(req,res)=>{
+    let data = {};
+    data.infosPage={
+        title:"",
+        description:"",
+        robots:"",
+        keywords:""
+    };
+    res.render('multimedia', data);
+});
+
+app.get('/bio',(req,res)=>{
+    let data = {};
+    data.infosPage={
+        title:"",
+        description:"",
+        robots:"",
+        keywords:""
+    };
+    res.render('bio', data);
+});
+
 
 app.listen(8000);
