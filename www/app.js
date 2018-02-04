677514b47eaf809f5d82ed1e74df261348acfa96
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-04 14:35:27 -0500

    Préparation du serveur

---
+++ www/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    var data = {test:"test1"}
+    res.render('index', data);
+});
+
+app.get('/web/:travail', (req,res)=>{
+    var data = {test:req.params.travail}
+    res.render('index', data);
+});
+
+app.listen(8000);
