e5fce985e1b58ad56c11b590d6dec0972f172f70
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-04 17:08:32 -0500

    Ajout

--- www/app.js
+++ www/app.js
@@ -1,14 +1,15 @@
+/*jshint esversion: 6 */
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
-    var data = {test:"test1"}
+    var data = {test:"test1"};
     res.render('index', data);
 });
 
 app.get('/web/:travail', (req,res)=>{
-    var data = {test:req.params.travail}
+    var data = {test:req.params.travail};
     res.render('index', data);
 });
 
