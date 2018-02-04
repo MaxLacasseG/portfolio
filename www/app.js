e8f9c4560f00472cb820583f14c50e208fcc488c
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-04 17:54:49 -0500

    Preparation du document de départ

--- www/app.js
+++ www/app.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + "/public"), function(req,res){
+    console.log(req.url);
+});
 
 app.get('/', (req,res)=>{
+    console.log(req.url);
     var data = {test:"test1"};
     res.render('index', data);
 });
