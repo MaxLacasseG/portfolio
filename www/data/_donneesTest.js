9b0482ab98c6b68915a778e59bfcdb80b603baf4
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-14 21:10:32 -0500

    Ajout d'un fichier de données de test

---
+++ www/data/_donneesTest.js
@@ -0,0 +1,23 @@
+[{
+    "_id": ObjectId("5a84e8927d35c0fe221cdfc0"),
+    "titre": "mon projet",
+    "categorie": "web",
+    "image": ["clashMenu-120x700.png"],
+    "description": "Lorem ipsum",
+    "tags": ["web", "site", "test"],
+    "like": 130,
+    "lien": "www.poirierbisson.com",
+    "temps": "20 hres",
+    "titre-court": "projet"
+}, {
+    "_id": ObjectId("5a84e89f7d35c0fe221cdfc1"),
+    "titre": "mon projet",
+    "categorie": "web",
+    "image": ["clashMenu-120x700.png"],
+    "description": "Lorem ipsum",
+    "tags": ["web", "site", "test"],
+    "like": 130,
+    "lien": "www.poirierbisson.com",
+    "temps": "20 hres",
+    "titre-court": "projet"
+}]
